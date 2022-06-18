35466f7139ad9611666ca634f8fc25de3ba604c5
shubham ubuntu <shubham.sutar@vinnovatetechnologies.com>
2022-06-18 19:22:44 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const bar = document.getElementById("bar");
+const menu = document.getElementById("menu");
+bar.addEventListener('click',function(){
+    // menu.classList.toggle("show");
+});
+bar.addEventListener('click',function(){
+     menu.classList.toggle("showh");
+});
+
